2bc2f81e47163864423675b5628e735d32aa3413
SanjokMangrati <smangrati25@gmail.com>
2024-09-08 20:45:04 +0530

    added animations to call to action

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,22 +1,48 @@
+"use client";
 import Button from "@/components/Button";
 import starsBg from "@/assets/stars.png";
 import gridLines from "@/assets/grid-lines.png";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const CallToAction = () => {
+  const sectionRef = useRef<HTMLElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+
+  const backgroundPositionY = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [-300, 300]
+  );
+
   return (
-    <section className="py-20 md:py-24">
+    <section className="py-20 md:py-24" ref={sectionRef}>
       <div className="container">
-        <div
+        <motion.div
           className="border border-white/15 py-24 rounded-xl overflow-hidden relative"
           style={{
             backgroundImage: `url(${starsBg.src})`,
+            backgroundPositionY,
+          }}
+          animate={{
+            backgroundPositionX: starsBg.width,
+          }}
+          transition={{
+            repeat: Infinity,
+            ease: "linear",
+            duration: 60,
           }}
         >
-          <div className="absolute inset-0 bg-[rgb(74,32,138)] bg-blend-overlay [mask-image:radial-gradient(50%_50%_at_50%_35%,black,transparent)]" style={{
-            backgroundImage: `url(${gridLines.src})`
-          }}></div>
+          <div
+            className="absolute inset-0 bg-[rgb(74,32,138)] bg-blend-overlay [mask-image:radial-gradient(50%_50%_at_50%_35%,black,transparent)]"
+            style={{
+              backgroundImage: `url(${gridLines.src})`,
+            }}
+          ></div>
           <div className="relative">
-
             <h2 className="text-5xl md:text-6xl tracking-tighter text-center font-medium max-w-sm mx-auto">
               AI driven SEO for everyone.
             </h2>
@@ -27,7 +53,7 @@ export const CallToAction = () => {
               <Button text="Join Waitlist" />
             </div>
           </div>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
